c29cb0b94fcded4b4d8fef32a497eda31e8beb52
Armaan Gupta <guptaarmaan774@gmail.com>
2021-02-21 16:12:01 +0530

    Added The Test Route!

--- routes/routes.api.js
+++ routes/routes.api.js
@@ -1,3 +1,9 @@
 const Router = require("express").Router()
 
+Router.get("/", (req, res, next) => {
+    res.json({
+        message: `🚀 Api Working Start Editing ${__dirname} `
+    })
+})
+
 module.exports = Router
